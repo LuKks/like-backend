5fe84ce05d3a62ef967df8bc2268f6b5f6d803c5
Lucas Barrena <lucasbarrena98@gmail.com>
2023-12-29 23:22:34 -0300

    Return request helper instead of backend instance

--- test/test.js
+++ test/test.js
@@ -3,9 +3,26 @@ const fetch = require('like-fetch')
 const launch = require('./app.js')
 
 test('basic', async function (t) {
+  const request = await launch(t)
+
+  const data = await request('/api/example')
+
+  t.is(data, 'Hello world!')
+})
+
+test('basic', async function (t) {
   const backend = await launch(t)
 
-  const response = await fetch('http://127.0.0.1:' + backend.port + '/api/example')
+  const response = await fetch('http://' + backend.host + ':' + backend.port + '/api/example')
+  const data = await response.json()
+
+  t.is(data, 'Hello world!')
+})
+
+test('basic', async function (t) {
+  const request = await launch(t)
+
+  const response = await request('/api/example', { responseType: false })
   const data = await response.json()
 
   t.is(data, 'Hello world!')
