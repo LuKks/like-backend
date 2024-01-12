fe465b5962416df2b670d9675bda61657aa678b0
Lucas Barrena <lucasbarrena98@gmail.com>
2024-01-11 23:00:04 -0300

    Remove unnecessary tests from test2

--- test/test2.js
+++ test/test2.js
@@ -1,29 +1,10 @@
 const test = require('brittle')
-const fetch = require('like-fetch')
 const launch = require('./app2.js')
 
 test('basic', async function (t) {
   const request = await launch(t)
 
   const data = await request('/api/example')
-
-  t.is(data, 'Hello world!')
-})
-
-test('basic', async function (t) {
-  const request = await launch(t)
-
-  const response = await request('/api/example', { responseType: false })
-  const data = await response.json()
-
-  t.is(data, 'Hello world!')
-})
-
-test('basic', async function (t) {
-  const backend = await launch(t)
-
-  const response = await fetch('http://' + backend.host + ':' + backend.port + '/api/example')
-  const data = await response.json()
 
   t.is(data, 'Hello world!')
 })
