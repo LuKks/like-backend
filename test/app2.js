79fd1f621d13a5872d3162da2a7c5adf66a796a6
Lucas Barrena <lucasbarrena98@gmail.com>
2024-01-16 10:50:20 -0300

    Add static `Backend.goodbye(onclose)`

--- test/app2.js
+++ test/app2.js
@@ -6,16 +6,14 @@ module.exports = Backend.launch(main)
 function main () {
   const app = express()
 
-  app.get('/api/example', function (req, res) {
-    res.json('Hello world!')
-  })
+  app.get('/api/example', require('./route2.js'))
 
   const server = app.listen(Backend.testing ? 0 : 1337, '127.0.0.1')
 
   return new Backend({
     server,
     goodbye: function () {
-      console.log('Goodbye')
+      console.log('App goodbye')
     }
   })
 }
